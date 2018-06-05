afa58159aa4535a9da2a9d41685beba425ace747
Fenix15100 <francisco.camacho15@gmail.com>
2018-06-05 12:59:27 +0200

    Refactor main code
    Refactor main class app
    Add suport to SQLITE

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,48 @@
-from flask import Flask,render_template
 import os
-from app.Articulos import Articulos
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-#Funcion que retorna mi aplicacion FLASK, la creo, registro los submodulos y especifico su configuracion
-#Segun la variable de entorno 'FLASK_ENV'
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydb.db"))
 def createapp():
 
     app = Flask(__name__)
-    app.register_blueprint(Articulos)
 
-    if os.environ['FLASK_ENV'] =='development':
+    app = registerModels(config(app))
 
-        app.config.from_mapping(
-            SQLALCHEMY_ECHO=True,
-            DEBUG=True,
-            SECRET_KEY='dev'
-
-
-        )
-    elif os.environ['FLASK_ENV'] =='production':
-        app.config.from_mapping(
-            SQLALCHEMY_ECHO=False,
-            DEBUG=False,
-            SECRET_KEY='prod'
-
-        )
+    db = SQLAlchemy(app)
 
     @app.route('/')
     def index():
         return render_template("home.html")
 
+    return app
+
+
+def config(app):
+    if os.environ['FLASK_ENV'] == 'development':
+
+        app.config.from_mapping(
+            SQLALCHEMY_ECHO=True,
+            DEBUG=True,
+            SECRET_KEY='dev',
+            SQLALCHEMY_DATABASE_URI=database_file,
+            SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+        )
+    elif os.environ['FLASK_ENV'] == 'production':
+        app.config.from_mapping(
+            SQLALCHEMY_ECHO=False,
+            DEBUG=False,
+            SECRET_KEY='prod',
+            SQLALCHEMY_DATABASE_URI=database_file,
+            SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+        )
+    return app
+
+def registerModels(app):
+    from app.Articulos import Articulos
+
+    app.register_blueprint(Articulos)
     return app
