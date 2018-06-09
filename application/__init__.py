04cb05a37d6972713efec4bfe61361653562b97f
fenix15100 <francisco.camacho15@gmail.com>
2018-06-10 01:00:11 +0200

    Me pego un tiro agaain

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,13 @@
 import os
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from dotenv import load_dotenv
-from app.Controller import *
-
-
-
+from flask_sqlalchemy import SQLAlchemy
+from application.Controller import mainController,articulos
 
 
 #Load ENV system from file
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydb.db"))
 APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
@@ -21,17 +20,28 @@ app.config.from_mapping(
             SQLALCHEMY_ECHO=os.getenv('SQLALCHEMY_ECHO'),
             DEBUG=os.getenv('DEBUG'),
             SECRET_KEY=os.getenv('SECRET_KEY'),
-            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI'),
+            SQLALCHEMY_DATABASE_URI=database_file,
             SQLALCHEMY_TRACK_MODIFICATIONS=os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
         )
 
-app.register_blueprint(main)
+#BluePrints
+app.register_blueprint(mainController)
+app.register_blueprint(articulos)
+
+
+#####
+
+db=SQLAlchemy(app)
+
+
 
 #Run app
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     app.run()
+
+
+
 
 
 
